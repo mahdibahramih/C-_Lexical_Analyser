c8996941e7d12a50389c62af2400e0ad8dc4e3ee
mahdi <mahdi@mahdibahramih@mahdibh.com>
2019-06-30 18:33:59 -0400

    add key_word , operator , sampleCode files

---
+++ code.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include < iostream > std;
+using namespace std;
+void main ()
+    {
+    int a , b , c , max , min ;
+    cout << "adadravaredkonid:" << endl;
+    cin >> a ;
+    cin >> b ;
+    cin >> c ;
+    max = a ;
+    min = a ;
+    if ( b > max )
+        max = b ;
+    if ( b < min )
+        {
+            min = b ;
+        }
+    if( c > max )
+    max = c ;
+    if ( c < min )
+        min = c ;
+    cout << "maxis:" << max<< endl ;
+    cout << "minis:" << min ;
+    cin.get();
+    a = 2222
+    b = 22.22
+    cin.ignore();
+/*a*/
+}
